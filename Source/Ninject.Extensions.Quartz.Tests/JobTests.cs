2c6dfe1fa9f2a93cd6fc8ceb3bcc94b8a98b4931
unknown <mark.huber@WIN-KI55HGCLDLD.dt.inc>
2015-05-25 12:04:46 -0500

    Updated to latest versions of Quartz/Ninject. Added test suite

---
+++ Source/Ninject.Extensions.Quartz.Tests/JobTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using NUnit.Framework;
+using Ninject.Modules;
+using Quartz;
+using Quartz.Spi;
+
+namespace Ninject.Extensions.Quartz.Tests
+{
+    [TestFixture]
+    public class JobTests
+    {
+        private IKernel _kernel;
+        private static bool _hasRun;
+
+        [SetUp]
+        public void Setup()
+        {
+            _kernel = new StandardKernel();
+        }
+
+        [Test]
+        public void TestKernelCanInstantiateScheduler()
+        {
+            var scheduler = _kernel.Get<IScheduler>();
+            Assert.IsNotNull(scheduler);
+        }
+
+        [Test]
+        public void TestHarnessCanCreateJob()
+        {
+            var scheduler = _kernel.Get<IScheduler>();
+
+            IJobDetail testJob = JobBuilder
+                                    .Create<TestJob>()
+                                    .Build();
+
+            ITrigger runOnce = TriggerBuilder.Create().WithSimpleSchedule(builder => builder.WithRepeatCount(0)).Build();
+            
+            scheduler.ScheduleJob(testJob, runOnce);
+
+            scheduler.Start();
+
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(5.0));
+
+            Assert.IsTrue(_hasRun);
+        }
+
+        public class TestJob : IJob
+        {
+            public void Execute(IJobExecutionContext context)
+            {
+                _hasRun = true;
+            }
+        }
+
+    }
+}
